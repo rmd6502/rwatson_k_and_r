79c4be3197693f4b071dc8d75955ef78c5174710
Robert Diamond <rmd6502@gmail.com>
2019-07-11 20:59:24 -0700

    add rob watsons answer to 1.9

---
+++ rwatson_19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// i'm surmising that this bit of code forms the basis for parsing input generally
+// you fundamentally need to keep track of the current input and previous input
+// in order to "see ahead".
+
+int main () {
+
+        int current, previous;
+        
+        previous = EOF;
+        
+        while ((current = getchar()) != EOF) {
+            if (current != ' ')
+                putchar(current);
+            
+            if (current == ‘ ' )
+                if (previous != ‘ ' )
+                    putchar(' ');
+            
+            previous = current;
+        }
+    
+}
